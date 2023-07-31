8e180b4c21a1fa53589bd0d2da1359e3bd8137e7
TSossa <133424718+TSossa@users.noreply.github.com>
2023-07-31 05:14:42 -0400

    Add files via upload

---
+++ CryptoManagerGFATestTest.java
@@ -0,0 +1,41 @@
+package third;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * This class represents GFA test cases for a CryptoManager object.
+ * 
+ * @author Farnaz Eivazi
+ * @version 7/12/2022
+ * 
+ */
+public class CryptoManagerGFATestTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testStringInBounds() {
+		assertTrue(CryptoManager.stringInBounds("THIS TEST SHOULD SUCCEED"));
+	}
+
+	@Test
+	public void testEncryptCaesar() {
+		assertEquals("IFMMP!XPSME", CryptoManager.encryptCaesar("HELLO WORLD", 1));
+	}
+
+	@Test
+	public void testDecryptCaesar() {
+		assertEquals("HELLO WORLD", CryptoManager.decryptCaesar("IFMMP!XPSME", 1));
+	}
+}
